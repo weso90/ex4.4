c99f17f84b8a32485c93b2d20a9809a86d8a64b0
Maksym Wesolowski <weso.pl@gmail.com>
2025-04-15 18:21:16 +0200

    changed print function to logging

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
 
 def get_number(number):
@@ -6,7 +8,7 @@ def get_number(number):
         try:
             return float(value)
         except ValueError:
-            print("Podana wartość nie jest liczbą!")
+            logging.info("Podana wartość nie jest liczbą!")
 
 def get_numbers():
     numbers = []
@@ -19,31 +21,31 @@ def get_numbers():
             number = float(value)
             numbers.append(number)
         except ValueError:
-            print("Podana wartość nie jest liczbą!")
+            logging.info("Podana wartość nie jest liczbą!")
     return numbers
 
 if operation == 1:
     summing = get_numbers()
     c = sum(summing)
-    print(f"Dodaję liczby {summing}")
+    logging.info(f"Dodaję liczby {summing}")
     
 elif operation == 2:
     a = get_number("Podaj składnik 1. ")
     b = get_number("Podaj składnik 2. ")
     c = a - b
-    print(f"Odejmuję {a} i {b}")
+    logging.info(f"Odejmuję {a} i {b}")
 
 elif operation == 3:
     multipling = get_numbers()
     c = 1
     for i in multipling:
         c*= i
-    print(f"Mnożę liczby {multipling}")
+    logging.info(f"Mnożę liczby {multipling}")
 
 elif operation == 4:
     a = get_number("Podaj składnik 1. ")
     b = get_number("Podaj składnik 2. ")
     c = a / b
-    print(f"Dzielę {a} i {b}")
+    logging.info(f"Dzielę {a} i {b}")
 
-print(f"Wynik to: {c}")
+logging.info(f"Wynik to: {c}")
